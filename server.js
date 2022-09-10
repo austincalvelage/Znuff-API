a68eb729ed9f7d8705aafa8fef0b25dcfb7094a5
Max <maxroswerk@gmail.com>
2022-09-11 01:57:43 +0200

    Change PORT name

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/animal.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
